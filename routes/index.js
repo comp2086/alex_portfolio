e1e5e9d9f2c6575447e7f82de89248eceb530122
Alex Andriishyn <Oleksandr.Andriishyn@MyGeorgian.ca>
2015-10-04 18:47:08 -0400

    Add about page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Portfolio' });
+});
+
+/* about page */
+router.get('/about', function(req, res, next) {
+    res.render('about');
 });
 
 module.exports = router;
