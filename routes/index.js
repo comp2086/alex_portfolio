07f3e20886c9337231631b2fa9bea62e07eeb202
Alex Andriishyn <Oleksandr.Andriishyn@MyGeorgian.ca>
2015-10-03 20:20:51 -0400

    Add bootstrap

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Alex Andriishyn portfolio' });
+  res.render('index', { title: 'Portfolio' });
 });
 
 module.exports = router;
