ec2bc79da52c9d9499957a0e11b6e02c2f6fd724
Alex Andriishyn <Oleksandr.Andriishyn@MyGeorgian.ca>
2015-10-04 19:50:25 -0400

    Adjust CSS, Add views

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,16 @@ router.get('/about', function(req, res, next) {
     res.render('about', { title: 'About me' });
 });
 
+/* projects page */
+router.get('/projects', function(req, res, next) {
+    res.render('projects', { title: 'Projects' });
+});
+
+/* services page */
+router.get('/services', function(req, res, next) {
+    res.render('services', { title: 'Services' });
+});
+
+/* contact me page */
+
 module.exports = router;
