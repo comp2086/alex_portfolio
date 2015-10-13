72289e1ef54146ddc1c5217d7708454859be5a75
Alex Andriishyn <alex.andriishyn@icloud.com>
2015-10-12 21:38:43 -0400

    Add post route for email

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,45 @@
 /*
-File name: index.js
-Author: Alex Andriishyn
-Website: http://alexandriishyn.azurewebsites.net/
-File description: routes file
+    File name: index.js
+    Author: Alex Andriishyn
+    Website: http://alexandriishyn.azurewebsites.net/
+    File description: routes file
 */
+
 var express = require('express');
 var router = express.Router();
 
-/* Home page. */
+// Email library and server
+var nodemailer = require('nodemailer');
+var transport = nodemailer.createTransport("SMTP", {
+                        service: "Gmail",
+                        auth: {
+                                user: "comp2086@gmail.com",
+                                pass: "1234Test"
+                              }
+});
+
+
+// Home page
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
-/* About page */
+// About page
 router.get('/about', function(req, res, next) {
     res.render('about', { title: 'About me' });
 });
 
-/* Projects page */
+// Projects page
 router.get('/projects', function(req, res, next) {
     res.render('projects', { title: 'Projects' });
 });
 
-/* Services page */
+// Services page
 router.get('/services', function(req, res, next) {
     res.render('services', { title: 'Services' });
 });
 
-/* Contact me page */
+// Contact me page
 router.get('/contactme', function(req, res, next) {
     res.render('contactme', { title: 'Contact me' });
 });
@@ -38,6 +50,19 @@ router.post('/contactme', function(req, res) {
     var message = req.body.message;
     
     // Send email
+    transport.sendMail({
+        from: email,
+        to: "alex.andriishyn@icloud.com",
+        subject: "Portfolio message from " + name + ", " + email,
+        text: message
+        }, function (err, res) {
+            if(err) {
+                console.log(err);
+            }
+            else {
+                document.getElementById("emailSent").innerHTML = "Your message has been sent";
+            }
+        }
+)});
     
-});
 module.exports = router;
