18c570e33f8916223a7038c091f0bb01904d332b
Alex Andriishyn <alex.andriishyn@icloud.com>
2015-11-19 18:37:48 -0500

    Trying out azure and mongolab

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 //staging connection
-module.exports = { 'url':'mongodb://localhost/userDB' };
+//module.exports = { 'url':'mongodb://localhost/userDB' };
 //live connection
-//module.exports = { 'url' : 'mongodb://auth_assignment:12345@ds031651.mongolab.com:31651/userdb' };
+module.exports = { 'url' : 'mongodb://auth_assignment:12345@ds031651.mongolab.com:31651/userdb' };
