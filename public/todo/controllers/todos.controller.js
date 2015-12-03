3f0ecf3f16cfbb1544542786999f1bb5acdee0a0
Alex Andriishyn <alex.andriishyn@icloud.com>
2015-12-02 19:39:55 -0500

    Add controllers

--- public/todo/controllers/todos.controller.js
+++ public/todo/controllers/todos.controller.js
@@ -1,1 +1,65 @@
-angular.module('todos').controller('TodoController', [])
+
+angular.module('todos').controller('TodoController', ['$scope', 'Todos', function ($scope, Todos) {
+  $scope.editing = [];
+  $scope.username = '';
+  $scope.userTodos = [];
+  $scope.setUserName = function (userName) {
+    $scope.username = userName; //get the username
+    $scope.todos = Todos.query(function () {
+      $scope.userTodos = []; // reset the userTodos array
+      $scope.todos.forEach(function (todo) {
+        if (todo.username == $scope.username) {
+          $scope.userTodos.push(todo);
+        }
+      });
+      $scope.todos = $scope.userTodos;
+    });
+  };
+  $scope.save = function () {
+    if (!$scope.newTodo || $scope.newTodo.length < 1) {
+      return;
+    }
+    var todo = new Todos({ name: $scope.newTodo, username: $scope.username, completed: false });
+    todo.$save(function () {
+      $scope.todos.push(todo);
+      $scope.newTodo = ''; // clear textbox
+    });
+  };
+  $scope.update = function (index) {
+    var todo = $scope.todos[index];
+    Todos.update({ id: todo._id }, todo);
+    $scope.editing[index] = false;
+  };
+  $scope.edit = function (index) {
+    $scope.editing[index] = angular.copy($scope.todos[index]);
+  };
+  $scope.cancel = function (index) {
+    $scope.todos[index] = angular.copy($scope.editing[index]);
+    $scope.editing[index] = false;
+  };
+  $scope.remove = function (index) {
+    var todo = $scope.todos[index];
+    Todos.remove({ id: todo._id }, function () {
+      $scope.todos.splice(index, 1);
+    });
+    $scope.editing[index] = false;
+  };
+  $scope.remainingTodos = function () {
+    var count = 0;
+    angular.forEach($scope.todos, function (todo) {
+      if ($scope.username == todo.username) {
+        count += todo.completed ? 0 : 1;
+      }
+    });
+    return count;
+  };
+  $scope.totalTodos = function () {
+    var count = 0;
+    angular.forEach($scope.todos, function (todo) {
+      if ($scope.username == todo.username) {
+        count++;
+      }
+    });
+    return count;
+  };
+}]);
--- public/todo/services/todo.js
+++ public/todo/services/todo.js
@@ -1,5 +1,5 @@
 angular.module('todos').factory('Todos', ['$resource', function($resource) {
   return $resource('/todos/:id', null, {
-
+    'update': { method: 'PUT' }
   });
 }]);
